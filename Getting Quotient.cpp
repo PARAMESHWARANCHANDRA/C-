0f4c40e78a1b9a6a49a4705272a49500b7466282
parameshwaran <47638210+PARAMESHWARANCHANDRA@users.noreply.github.com>
2021-09-23 13:06:11 +0530

    Create Getting Quotient.cpp

---
+++ Getting Quotient.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout<< "Enter x =";
+    cin>>x;
+    cout<< "Enter y =";
+    cin>>y;
+    int QUO=x/y;
+    cout<<"QUO ="<<QUO;
+
+    return 0;
+}
