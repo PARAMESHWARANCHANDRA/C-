d47c8625e5008372621b8e4dbccadca9401ceb35
parameshwaran <47638210+PARAMESHWARANCHANDRA@users.noreply.github.com>
2021-09-23 13:01:21 +0530

    Create Multiply Two numbers.cpp

---
+++ Multiply Two numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout<< "Enter x =";
+    cin>>x;
+    cout<< "Enter y =";
+    cin>>y;
+    int MUL=x*y;
+    cout<<"MUL ="<<MUL;
+
+    return 0;
+}
