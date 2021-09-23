7aaa8f4fabb55b36002a58b4731d4ec2cc1cd62c
parameshwaran <47638210+PARAMESHWARANCHANDRA@users.noreply.github.com>
2021-09-23 12:45:49 +0530

    Create Subtracting two numbers.cpp

---
+++ Subtracting two numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout<< "Enter x =";
+    cin>>x;
+    cout<< "Enter y =";
+    cin>>y;
+    int DIFF=x-y;
+    cout<<"DIFF ="<<DIFF;
+
+    return 0;
+}
