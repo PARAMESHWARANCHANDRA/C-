18783d2fa0359822ebd95b051d98e67c8fbd4d36
parameshwaran <47638210+PARAMESHWARANCHANDRA@users.noreply.github.com>
2021-09-23 12:43:48 +0530

    Create Adding Two numbers.cpp

---
+++ Adding Two numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout<< "Enter x =";
+    cin>>x;
+    cout<< "Enter y =";
+    cin>>y;
+    int SUM=x+y;
+    cout<<"SUM ="<<SUM;
+
+    return 0;
+}
