84671b871f2f16177b363a2ef30f6e3b378ab94b
parameshwaran <47638210+PARAMESHWARANCHANDRA@users.noreply.github.com>
2021-09-29 19:08:22 +0530

    Create Check whether the number is positive or negative.cpp

---
+++ Check whether the number is positive or negative.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter number:"<<endl;
+    cin>> number;
+    
+    if(number>=0){
+        cout<<"The number entered is postive"<<endl;
+    }else{
+        cout<<"The number entered is negative"<<endl;
+    }
+    cout<<"This process is executed";
+
+    return 0;
+}
