792cd2fc100508627bfaa95dc22221b91f6dc6ba
parameshwaran <47638210+PARAMESHWARANCHANDRA@users.noreply.github.com>
2021-09-29 13:12:09 +0530

    Create Print the number entered.cpp

---
+++ Print the number entered.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<< "Enter the number:"<< endl;
+    cin>> number;
+    cout<< "The number your entered:"<< number;
+    
+    return 0;
+}
